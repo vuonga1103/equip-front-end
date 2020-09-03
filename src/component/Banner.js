9c0813ce4c3e8cc920572f3c8a2e31b278820999
vuonga1103 <vuonga1103@yahoo.com>
2020-09-02 22:23:19 -0400

    roughly styled Banner and NavBar

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Banner = () => {
-  return <div>I AM THE BANNER</div>;
+  return (
+    <div class="ui header">
+      <div class="ui huge header">
+        <img
+          src="https://img.icons8.com/cotton/64/000000/electric-wheelchair.png"
+          alt="Equip Logo, a wheelchair"
+        />
+        EQUIP
+      </div>
+      <div class="sub header">
+        An Online Market Place for New and Used Durable Medical Equipment
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-// Takes in prop item, which is an item object, and returns ItemPage for that item
 const NavBar = () => {
-  return <div>I AM THE NAV BAR</div>;
+  return (
+    <nav className="ui huge menu">
+      <div className="item">Home</div>
+      <div className="item">About</div>
+      <div className="right menu">
+        <div className="item">Log In</div>
+        <div className="item">
+          <div className="ui primary button">Sign Up</div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
