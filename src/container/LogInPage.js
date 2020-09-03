7a880a75b4f1497fef215eb9ae91d186b900f1ee
vuonga1103 <vuonga1103@yahoo.com>
2020-09-03 12:50:57 -0400

    styled login page

--- src/container/LogInPage.js
+++ src/container/LogInPage.js
@@ -3,7 +3,21 @@ import "../styling/LogInPage.css";
 
 class LogInPage extends React.Component {
   render() {
-    return <div>hello from login page</div>;
+    return (
+      <form className="ui form">
+        <div className="field">
+          <label>USERNAME</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div className="field">
+          <label>PASSWORD</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <button className="ui button" type="submit">
+          Log In
+        </button>
+      </form>
+    );
   }
 }
 
